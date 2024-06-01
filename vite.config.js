fa263cd35d677b383329a3c13f8319ea63996c6e
vincent <vincentcharlebois@gmail.com>
2024-06-01 12:18:28 -0400

    configs from speedrun

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 export default defineConfig({
 	plugins: [sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
-	}
+	},
+	preprocess: vitePreprocess()
 });
