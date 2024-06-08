133a9864d18c209e1252d5c24878bdee1ae792e9
vincent <vincentcharlebois@gmail.com>
2024-06-07 20:57:41 -0400

    🔥

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,12 @@
 	import { t } from 'svelte-i18n';
 </script>
 
+<svelte:head>
+	<title>
+		Vincent Charlebois - {$t('basic_title')}
+	</title>
+</svelte:head>
+
 <main class="flex flex-col place-items-center justify-center overflow-hidden">
 	<h2 class="italic">
 		{$t('basic_title')}
