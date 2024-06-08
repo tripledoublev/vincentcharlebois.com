3464ec5d41d1132fbd57004e1fc24dd565a26c69
vincent <vincentcharlebois@gmail.com>
2024-06-07 20:41:09 -0400

    Fix cap

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -5,6 +5,6 @@
 <main>
 	<p class="m-2 px-4 md:m-6 md:px-8 text-left">
 		{$t('about')}
-		<a class="font-extrabold hover:underline rounder-md" href="https://hypha.coop">HYPHA</a>
+		<a class="font-extrabold hover:underline rounder-md" href="https://hypha.coop">Hypha</a>
 	</p>
 </main>
