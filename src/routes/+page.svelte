a023aa663bda5d105be1c32f47f811c70321c1c3
vincent <vincentcharlebois@gmail.com>
2024-06-07 20:41:26 -0400

    center titles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
 	import { t } from 'svelte-i18n';
 </script>
 
-<main class="flex flex-col place-items-center">
+<main class="flex flex-col place-items-center justify-center overflow-hidden">
 	<h2 class="italic">
 		{$t('basic_title')}
 	</h2>
@@ -10,3 +10,10 @@
 		{$t('basic')}
 	</p>
 </main>
+
+<style>
+	main {
+		width: 100%;
+		height: 75vh;
+	}
+</style>
