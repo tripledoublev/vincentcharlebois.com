ef1d86b9ec70044e5547bfc4f5f82e369edcd499
vincent <vincentcharlebois@gmail.com>
2024-06-07 20:41:49 -0400

    init i18n

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,19 +1,28 @@
 <script>
+	import { onMount } from 'svelte';
+	import { fade } from 'svelte/transition';
 	import Nav from '$components/Nav.svelte';
-	import { locale, init, getLocaleFromNavigator } from 'svelte-i18n';
+	import { locale } from 'svelte-i18n';
 	import '../app.css';
-	import { initializeI18n } from '$lib/i18n.js';
+	import { initializeI18n, loadTranslations } from '$lib/i18n.js';
 
+	let loading = true;
+
+	
 	initializeI18n();
-	console.log('Hello from src/lib/index.js!');
+  console.log('Initializing i18n...');
 
-	init({
-		fallbackLocale: 'en',
-		initialLocale: getLocaleFromNavigator()
-	});
+  onMount(async () => {
+	
+    await loadTranslations();
+    loading = false;
+  });
 
 	$: locale.set($locale || 'en');
 </script>
-
+{#if !loading}
+<div transition:fade={{ delay: 250, duration: 250 }}>
 <Nav />
 <slot />
+</div>
+{/if}
