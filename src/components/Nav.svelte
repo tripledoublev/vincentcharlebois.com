75ea2502dc7c07427651004474229550448ad515
vincent <vincentcharlebois@gmail.com>
2024-06-11 23:18:37 -0400

    Reorder Nav

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -5,27 +5,25 @@
 	import Links from './Links.svelte';
 </script>
 
-<nav class="w-full flex-row flex-wrap items-center px-2 py-3">
+<nav class="w-full px-2 py-3">
 	<div
-		class="flex md:flex-row container justify-between px-2 mx-auto flex-wrap items-center content-around"
+		class="container mx-auto flex flex-wrap items-center justify-around md:justify-between px-2 relative"
 	>
-		<div class="order-3 my-1 lg:mx-[5vw] items-center">
-			<Switch />
-		</div>
-		<div class="order-1 my-1 lg:mx-[5vw] items-center">
-			<ColorSwitcher />
-		</div>
-		<div
-			class="order-4 lg:order-2 w-full lg:mx-[5vw] lg:mr-12 relative flex lg:w-auto lg:static lg:block lg:justify-end justify-center"
-		>
-			<Links />
-		</div>
-		<div
-			class="order-2 lg:order-5 lg:mx-[5vw] lg:ml-12 relative flex lg:w-auto lg:static lg:block lg:justify-start justify-center"
-		>
+		<div class="order-1 flex items-center lg:ml-12 z-20">
 			<a href="/" class:active={$page.url.pathname === '/'}>
 				<h1>vincent charlebois</h1>
 			</a>
+		</div>
+		<div class="order-3 flex items-center z-10 lg:mr-12">
+			<div class="my-1 mx-3">
+				<Switch />
+			</div>
+			<div class="my-1 mx-3">
+				<ColorSwitcher />
+			</div>
+		</div>
+		<div class="relative lg:absolute order-3 inset-0 flex justify-center items-center">
+			<Links />
 		</div>
 	</div>
 </nav>
