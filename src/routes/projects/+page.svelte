114aa344b41fd68eb6ab45f0728e09899513aa0a
vincent <vincentcharlebois@gmail.com>
2024-06-11 23:20:40 -0400

    Normalize main's min height

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -8,7 +8,7 @@
 	</title>
 </svelte:head>
 
-<main>
+<main class="flex flex-col place-items-center justify-center min-h-[75vh]">
 	<p class="m-1 my-2 px-4 md:m-6 md:px-8 text-left">
 		{$t('projects_A')}
 	</p>
@@ -16,16 +16,14 @@
 		{$t('projects_B')}
 		<a class="italic font-extrabold hover:underline" href="https://ffforests.xyz"
 			>fabricating future forests</a
-		>,
-		{$t('projects_ext')}
+		>{$t('projects_ext')}
 	</p>
 	<p class="m-1 my-2 px-4 md:m-6 md:px-8 text-left">
 		{$t('projects_ext1')}
 		<a
 			class="italic font-extrabold hover:underline"
 			href="https://vincentcharlebois.net//nodes.html">nodes</a
-		>
-		{$t('projects_ext2')}
+		>{$t('projects_ext2')}
 	</p>
 	<p class="m-1 my-2 px-4 md:m-6 md:px-8 text-left">
 		{$t('projects_now')}
