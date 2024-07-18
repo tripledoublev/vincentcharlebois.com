7975b6460d75e4fb71fce09222b289aed29bdb1d
vincent <vincentcharlebois@gmail.com>
2024-07-17 20:51:04 -0400

    🚧

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,10 +10,29 @@
 	</title>
 </svelte:head>
 
-<main class="flex flex-col place-items-center justify-center overflow-hidden w-full min-h-[75vh]">
+<main class="flex flex-col place-items-center justify-center overflow-hidden w-full min-h-[75vh] mt-12 md:mt-24"> 
+	<div class="flex md:hidden items-center lg:ml-12 z-20 mb-32">
+			<h1 class="text-center">vincent charlebois</h1>
+	</div>
+
+	
 	<h2 class="italic">
 		{$t('basic_title')}
 	</h2>
 
 	<Waves />
 </main>
+
+<style>
+	h1 {
+		font-size: 1.75rem;
+	}
+	@media (min-width: 568px) {
+		h1 {
+			font-size: 2.5rem;
+		}
+	}
+	main {
+		height: 80vh;
+	}
+</style>
