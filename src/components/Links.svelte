c2f87412e63bbdff27a0d5970ebfb63df7bd33fc
vincent <vincentcharlebois@gmail.com>
2025-03-31 23:06:18 -0400

    Refactor Links component to support localized URLs

--- src/components/Links.svelte
+++ src/components/Links.svelte
@@ -1,15 +1,28 @@
 <script>
 	import { t } from 'svelte-i18n';
 	import { page } from '$app/stores';
+	
+	// Accept the language prefix from the parent component
+	export let langPrefix = '/en';
+	
+	// Get localized URL slugs
+	$: aboutSlug = $t('url_slugs.about');
+	$: projectsSlug = $t('url_slugs.projects');
+	$: contactSlug = $t('url_slugs.contact');
+	
+	// Construct full paths
+	$: aboutPath = `${langPrefix}/${aboutSlug}`;
+	$: projectsPath = `${langPrefix}/${projectsSlug}`;
+	$: contactPath = `${langPrefix}/${contactSlug}`;
 </script>
 
 <div class="flex justify-end">
 	<div class="md:flex flex-grow items-center justify-center lg:justify-end z-50">
 		<ul class="flex md:flex-row list-none">
 			<a
-				href="/about"
+				href={aboutPath}
 				class="px-1 flex flex-grow items-center text-lg md:text-xl lowercase font-bold leading-snug hover:font-black"
-				class:active={$page.url.pathname === '/about/'}
+				class:active={$page.url.pathname.includes(aboutSlug)}
 			>
 				<li class="nav-item flex-auto">
 					{$t('linkabout')}
@@ -17,9 +30,9 @@
 			</a>
 
 			<a
-				href="/projects"
+				href={projectsPath}
 				class="px-1 flex items-center text-lg md:text-xl lowercase font-bold leading-snug hover:font-black"
-				class:active={$page.url.pathname === '/projects/'}
+				class:active={$page.url.pathname.includes(projectsSlug)}
 			>
 				<li class="nav-item flex-auto">
 					{$t('linkprojects')}
@@ -27,11 +40,13 @@
 			</a>
 
 			<a
-				href="/contact"
+				href={contactPath}
 				class="px-1 flex items-center text-lg md:text-xl lowercase font-bold leading-snug hover:font-black"
-				class:active={$page.url.pathname === '/contact/'}
+				class:active={$page.url.pathname.includes(contactSlug)}
 			>
-				<li class="nav-item flex-auto">Contact</li>
+				<li class="nav-item flex-auto">
+					{$t('linkcontact')}
+				</li>
 			</a>
 		</ul>
 	</div>
