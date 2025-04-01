fe160c0f74b3f2855d209f4f782d8b1b46e8c240
vincent <vincentcharlebois@gmail.com>
2025-03-31 21:44:05 -0400

    Enhance SEO

--- src/components/SEO.svelte
+++ src/components/SEO.svelte
@@ -3,19 +3,29 @@
   import { page } from '$app/stores';
   
   export let image = "https://vincentcharlebois.com/vincentcharlebois-com.png";
+  export let title = '';
+  export let description = '';
   
   // Determine the current URL if not provided
   $: currentUrl = `https://vincentcharlebois.com${$page.url.pathname}`;
 </script>
 
 <svelte:head>
-  <!-- Open Graph / Facebook -->
-  <meta property="og:type" content="website" />
-  <meta property="og:url" content={currentUrl} />
-  <meta property="og:image" content={image} />
-  
-  <!-- Twitter -->
-  <meta property="twitter:card" content="summary_large_image" />
-  <meta property="twitter:url" content={currentUrl} />
-  <meta property="twitter:image" content={image} />
+	<title>
+		{title}
+	</title>
+	<meta name="description" content={description} />
+
+	<meta property="og:title" content={title} />
+	<meta property="og:description" content={description} />
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content={currentUrl} />
+    <meta property="og:image" content={image} />
+
+    
+	<meta property="twitter:title" content={title} />
+	<meta property="twitter:description" content={description} />    
+    <meta property="twitter:card" content="summary_large_image" />
+    <meta property="twitter:url" content={currentUrl} />
+    <meta property="twitter:image" content={image} />
 </svelte:head> 
