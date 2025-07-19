b5db20398e16069a9628fdd0cb447a12c7d6c27c
vincent <vincentcharlebois@gmail.com>
2025-07-18 20:51:55 -0400

    🧠

--- src/routes/fr/projets/+page.js
+++ src/routes/fr/projets/+page.js
@@ -1,12 +1,13 @@
 export function load({ url }) {
-  const title = "Projets \Vincent Charlebois";
-  const description = "Explorez les projets de Vincent Charlebois, son travail au sein de la coopérative de travailleurs Hypha et ses projets artistiques.";
-  
-  return {
-    seo: {
-      title,
-      description,
-      currentUrl: `https://vincentcharlebois.com${url.pathname}`
-    }
-  };
-} 
+	const title = 'Projets Vincent Charlebois';
+	const description =
+		'Explorez les projets de Vincent Charlebois, son travail au sein de la coopérative de travailleurs Hypha et ses projets artistiques.';
+
+	return {
+		seo: {
+			title,
+			description,
+			currentUrl: `https://vincentcharlebois.com${url.pathname}`
+		}
+	};
+}
