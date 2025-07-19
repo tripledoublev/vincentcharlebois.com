430bdd4e5c59a7f31209d1a8f1b256b9055d267a
vincent <vincentcharlebois@gmail.com>
2025-07-18 20:51:59 -0400

    🌈

--- src/routes/publickey/+page.js
+++ src/routes/publickey/+page.js
@@ -1,12 +1,12 @@
 export function load({ url }) {
-  const title = "PGP Public Key \Vincent Charlebois";
-  const description = "Vincent Charlebois's PGP public key for secure communication.";
-  
-  return {
-    seo: {
-      title,
-      description,
-      currentUrl: `https://vincentcharlebois.com${url.pathname}`
-    }
-  };
-} 
+	const title = 'PGP Public Key Vincent Charlebois';
+	const description = "Vincent Charlebois's PGP public key for secure communication.";
+
+	return {
+		seo: {
+			title,
+			description,
+			currentUrl: `https://vincentcharlebois.com${url.pathname}`
+		}
+	};
+}
