4e4e55858f03d5971d67583589c9c417b3393b7f
vincent <vincentcharlebois@gmail.com>
2025-10-04 19:46:40 -0400

    🏠

--- src/routes/fr/+page.svelte
+++ src/routes/fr/+page.svelte
@@ -2,10 +2,7 @@
 	import SEO from '$components/SEO.svelte';
 	import { onMount } from 'svelte';
 
-	const seo = {
-		title: 'Vincent Charlebois',
-		description: 'Vincent Charlebois; artiste intermédiatique, développeur logiciel et membre de la Hypha Worker Co-operative.'
-	};
+	export let data;
 
 	let currentRole = '';
 	let roleIndex = 0;
@@ -197,7 +194,7 @@ Au sein de cette structure, Charlebois détient le titre d'« artiste-chercheur 
 	}
 </script>
 
-<SEO {...seo} />
+<SEO {...data.seo} />
 
 <main class="flex flex-col place-items-start justify-center overflow-hidden w-full min-h-[75vh]">
 	<div class="typewriter-container text-left px-2 w-full">
