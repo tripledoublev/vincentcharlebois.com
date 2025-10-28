1544278ed122647089c04c819d9936335f5dae4d
tripledoublev <tripledoublev@users.noreply.github.com>
2025-10-28 03:50:44 +0000

    deploy: d3848b460e8aa1971846bd646f74e57fb6ba641d

---
+++ _app/immutable/nodes/17.BLKtUkG7.js
@@ -0,0 +1,56 @@
+import{s as _,a as N,r as Y,t as F}from"../chunks/sXGDExf-.js";import{S as X,i as O,k as S,s as I,e as h,t as A,l as J,f as V,c as C,a as y,u as M,d as f,b as E,m as B,n as R,g,h as m,v as W,o as q,p as H,q as $}from"../chunks/5UdhpouY.js";import{g as ee,a as te}from"../chunks/CgU5AtxT.js";import{S as se}from"../chunks/vNgScYg6.js";function oe({url:a}){return{seo:{title:"PGP Public Key Vincent Charlebois",description:"Vincent Charlebois's PGP public key for secure communication.",currentUrl:`https://vincentcharlebois.com${a.pathname}`}}}const fe=Object.freeze(Object.defineProperty({__proto__:null,load:oe},Symbol.toStringTag,{value:"Module"}));function ae(a){let t;return{c(){t=A("Copy to clipboard")},l(s){t=E(s,"Copy to clipboard")},m(s,o){g(s,t,o)},d(s){s&&f(t)}}}function ne(a){let t;return{c(){t=A("Copied to clipboard!")},l(s){t=E(s,"Copied to clipboard!")},m(s,o){g(s,t,o)},d(s){s&&f(t)}}}function le(a){let t,s,o,l,r,w="Save as .asc file",j,i,d,n,p,v,x,k,P,U,L;const G=[a[1]];let K={};for(let e=0;e<G.length;e+=1)K=N(K,G[e]);t=new se({props:K});function Z(e,c){return e[0]?ne:ae}let D=Z(a),u=D(a);return{c(){S(t.$$.fragment),s=I(),o=h("main"),l=h("div"),r=h("button"),r.textContent=w,j=I(),i=h("button"),u.c(),d=I(),n=h("button"),p=h("pre"),v=A("            "),x=A(a[2]),k=A(`
+        `),this.h()},l(e){J(t.$$.fragment,e),s=V(e),o=C(e,"MAIN",{class:!0});var c=y(o);l=C(c,"DIV",{class:!0});var b=y(l);r=C(b,"BUTTON",{class:!0,"data-svelte-h":!0}),M(r)!=="svelte-10pnj3h"&&(r.textContent=w),j=V(b),i=C(b,"BUTTON",{class:!0});var z=y(i);u.l(z),z.forEach(f),b.forEach(f),d=V(c),n=C(c,"BUTTON",{class:!0});var T=y(n);p=C(T,"PRE",{});var Q=y(p);v=E(Q,"            "),x=E(Q,a[2]),k=E(Q,`
+        `),Q.forEach(f),T.forEach(f),c.forEach(f),this.h()},h(){B(r,"class","text-sm font-medium download svelte-y1lg3f"),B(i,"class","text-sm font-medium download svelte-y1lg3f"),B(l,"class","flex flex-col flex-wrap md:flex-row mt-2 md:mt-12"),B(n,"class","text-sm font-medium mt-12"),B(o,"class","flex flex-col place-items-center justify-center min-h-[75vh]")},m(e,c){R(t,e,c),g(e,s,c),g(e,o,c),m(o,l),m(l,r),m(l,j),m(l,i),u.m(i,null),m(o,d),m(o,n),m(n,p),m(p,v),m(p,x),m(p,k),P=!0,U||(L=[W(r,"click",a[4]),W(i,"click",a[3]),W(n,"click",a[3])],U=!0)},p(e,[c]){const b=c&2?ee(G,[te(e[1])]):{};t.$set(b),D!==(D=Z(e))&&(u.d(1),u=D(e),u&&(u.c(),u.m(i,null)))},i(e){P||(q(t.$$.fragment,e),P=!0)},o(e){H(t.$$.fragment,e),P=!1},d(e){e&&(f(s),f(o)),$(t,e),u.d(),U=!1,Y(L)}}}function re(a,t,s){const o={title:"PGP Public Key | Vincent Charlebois",description:"Vincent Charlebois PGP public key for secure communication."};let l=`
+-----BEGIN PGP PUBLIC KEY BLOCK-----
+
+mQINBGfD0WkBEADIzAp1FuL3PdsdQbefTVWThCu7F/z/9jisIqW4f9HS08eVY+Ui
+mytF3mF1T1q3sqaU+KzQHeCmq5yInHMjqT7QI/L64bLgz9DMYPj2NcPGBGprymEu
+tDbzvX4xFKXmNRp5lcetTnE8lsCUfCuy9dG22RNZcYIqUWrTyN7tW5/HDJhhV5tE
+EfCDZTVgPzjBCbckzM/RjtSqAowcmwscRGLIfbVTsf33jdY7vfae5/wdjawL5nPE
+BHinvEd4FjbKZb4+3AAVDVjAEhT77/IE2NRauFGIMmEQdH5QWzypUwHfnlw9XROB
+Q9HvmVwY28wxkXuA3dmM0JYSBLcuXYHp7bbXmWCiA3pqtWi/eZYTyvttVfOQPYkl
+PlwJKynknew+U2NkOdnrZBrhC4Duh7VmiIb0zdsK4A61H1wAm6e+OfTNvbPYChMw
+u6UULKu5Ouk0MTitzrGQil8tWmM31JPXL8QPWf3SGD7300Xf0PGa/Khfswf0YD60
+2aV5KIZ10M+QZMZ4J1lCrHsLSR3TGMiwtuD8P5FaxeSIovygf+wWYNgbKiIdUgMR
+FtOePWKI+0BpOGV3KhOH1bVOSf8WZVjXJuM5VaE0YRd/KREu2Bl+zMIMP0u7DBIT
+F6kLlEq9zEpmjkrgKOZxyvoTUZ98LlIIxi4qWrGorJxDl7qiBiB9Na3L6QARAQAB
+tDh2aW5jZW50IGNoYXJsZWJvaXMgKGVtYWlsKSA8dmluY2VudGNoYXJsZWJvaXNA
+Z21haWwuY29tPokCVwQTAQgAQRYhBJv762oTmS1/ajM91NlPVgsus6+WBQJnw9Fp
+AhsDBQkDwmcABQsJCAcCAiICBhUKCQgLAgQWAgMBAh4HAheAAAoJENlPVgsus6+W
+T1sQAIx7P/14V56xCKxNyLOnspBA1q6UYlUd2VhE0Gze9QvR1EAbPQfyb4NRW6mo
+6T+AcicsusY+IC7yvsaK8RcYmLgxA1CCXa0I3sAcjXqVtxpFynkcid6rRDxgTWDv
+WEv6ln04iWeTQ8UeM/jnSKH5EZE7/GZsE7ZKG2wIUI7T2Ec/tFby4ycQwWnYyeP2
+eDHXh1rjDLQPyC8hGR9czoLzXTSsjqw3jlZ2rjGq7KaXTvmdLW582z+ftV/DBJQm
+BNdwIIvOCk8wS052dliiZ/zkjGWEiCcyo5r88+DmRCLfJjtjz7NybCIOR6quKkKv
+ejVzvTyk53ue+w69Q95NxUrjy5rKdjeyZ8bxizlfzF2dmgR5aiMA4XQybSRzPU8n
+X1cF71QBgeU3j6vmEjwy7LxbPS0Cw7VgfXnx+GB1JAsIPorAxyivNXO8psmOSiZD
+YCwFzVYdFSm28kfkecMC7upFD2aEw0/OKqG6sDkIkEOZa6tEjf+KX7sXb/BVIdB5
+4er32t9Uw1Y6FRddqg8gvzm0XHVj8LwNFEr+pUX34sVKE2hVpitjDdNaXRr8URBL
+t/DQvx1ebxq8l/CI8stWvQbiY8m7zgvGjCm4oHEfZZQviMzCfbfr2jGJFIEjPFZD
+K3KktesdoxBjNekZDt8X1j/APOGe83f1gVvtFgWEmF1xQyVmuQINBGfD0WkBEADn
+GxDYl4hOnkxuDVJWlwGrCrt9XFI8odtsU+jYp/DU3x3oGt3H8ogPVcjmngBjnX99
+SyuSqtHEhfHH+eYG1mlge/NPD0teeb3pZlG8xQW9jOMGi8h5YiMwOy5jX2CvpNKJ
++AIdRpnHqNg4Kk7aYQCt7a/ZYmWVVKrOPla/SOMuIofdZ+hsEcW44yCmke5BW/U3
+rTg+8/3inBJlbAKQUQ0nf9GMSjmWaa/Qj88eG6jRR8sl0xICjBvWjqG8f9oDbbpW
+uLzt0ELd2hUJuL+8DSmNMWjTzkRmxLoU0x6beSj5zsqh2lxplroJmgmWwwK+PRSc
+W4UBKJVrZbt8k8JtYT3osQj2aXBbqX3brdgB3PpegdOQXbpiPekE/xyoJNntELT0
+15VZLJdo/rSbFf8whakf2Ff5xUKpcsz38Be7OFL7HIpj8rYMJQXSF5Gv1pUwGqA9
+sxrDcDPwFOQGeqL2U42Hi0+PDBPH25dMkm+sYLfr8aFnrU8ICA6/UAaVDFz0ZwJ4
+sA1SzpDpYMZ4U0ySVaKIg5Q6ac0u+s7DHim93bV5DljAfhSJKmmfyhDwDxBANILF
+uO9CxJXESRAJEIAYUdxFQFx3uwaTExoJiA6N8xXJqF3JF9ZYtpdm4hgzk+HxSGqc
+fSO22RHLL/BDRUUf2k0nO4irEK1NhlDGBroTZhJmFwARAQABiQI8BBgBCAAmFiEE
+m/vrahOZLX9qMz3U2U9WCy6zr5YFAmfD0WkCGwwFCQPCZwAACgkQ2U9WCy6zr5bt
+QBAApglJiJfN4x26Dy+Yx4cZcjaKsMN0UnTZhfzPeY2Cj4tRlTYl5oVBmylvIeFs
+8Uiww9LPqjV1rYjplbBxquQszDocCKJ1aNapqtEpc2cj8Axz7SSB+QZjhq6rnZVN
+K2QNEE/18HnIfWwzjvKAQJ6PtcUEJ4kXfMtcaD1tuNyaB5BhJxQEZj96ovvi3+Qx
+fzDgscLuEjBrLse7yQTK06uUXAqxcojEFsz3mG6WnHKxcrzhQB0FB/03/huuPCyC
+Ph2/NynDLpj/3iosoagqxGs7KbI8iyMc/lla972hE+HWx5/z4e8ZfhMWHHBChmue
+kD/25lseTwyHN8cVp5c5n6wTYOErZ/zP7e493DKsQNkqYq0SA1GRCDjzcXV7HkPZ
+SIQTmRIlPnMoSUfF3gIldIKOXLZnKmrRZ4cTpdDfVTQ9f2wdK8Cx0VGtsmMx/PpI
+DK0vkeUfoOP5UpGhWLG+eiX8X26+k0Z7NuLnO3zhtpok+4vG+15E24xhc70EX2GU
+8wpLzBB99WTfyMOlqUuestCKPI6ulbq5gTnDLaud9Afr5gqBoXjr0gmGhmsMs0A9
+zr8pBXYkVd8JonsrWtWV+8MLkd9SXR7bgHoSD3j1ZYp+7C46htrVIKFFWH/iF2Dw
+kJ6UNvOeaLglww0AZQ/ZQGjO3IS0pgmDkkGrKBMyst3tGz0=
+=U/CU
+-----END PGP PUBLIC KEY BLOCK-----
+    `,r=!1;async function w(){try{await navigator.clipboard.writeText(l),s(0,r=!0),await F(),setTimeout(()=>{s(0,r=!1)},2e3)}catch{}}function j(){const i=new Blob([l],{type:"text/plain"}),d=URL.createObjectURL(i),n=document.createElement("a");n.href=d,n.download="vincent_charlebois_public_key.asc",document.body.appendChild(n),n.click(),document.body.removeChild(n),URL.revokeObjectURL(d)}return[r,o,l,w,j]}class pe extends X{constructor(t){super(),O(this,t,re,le,_,{})}}export{pe as component,fe as universal};
