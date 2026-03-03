f740b2be207f7657c8d46fa1787e6707bba67850
vincent <vincentcharlebois@gmail.com>
2026-03-03 00:45:37 -0500

    🌿

--- src/routes/fr/contact/+page.svelte
+++ src/routes/fr/contact/+page.svelte
@@ -5,7 +5,7 @@
 	const seo = {
 		title: 'Contact | Vincent Charlebois',
 		description:
-			'Contactez Vincent Charlebois - artiste intermédiatique, technologue créatif et membre de la Hypha Worker Co-operative.'
+			'Contactez Vincent Charlebois — architecte de systèmes IA et traducteur recherche-produit. Trouvez des liens vers GitHub, LinkedIn, CV et plus.'
 	};
 
 	onMount(() => {
@@ -75,19 +75,6 @@
 	<div class="px-5 text-center">
 		<p class="text-2xl text-left px-5 pt-7 pb-7 mb-1 w-80">Liens:</p>
 		<ul class="flex flex-row flex-wrap">
-			<a class="text-sm font-medium contact-links" target="_blank" href="https://hypha.coop/">
-				<li class="py-2">hypha.coop</li>
-			</a>
-			<a
-				class="text-sm font-medium contact-links"
-				target="_blank"
-				href="mailto:vincent@charlebois.info"
-			>
-				<li class="py-2">email</li>
-			</a>
-			<a class="text-sm font-medium contact-links" href="/publickey">
-				<li class="py-2">pgp</li>
-			</a>
 			<a
 				class="text-sm font-medium contact-links"
 				target="_blank"
@@ -95,68 +82,8 @@
 			>
 				<li class="py-2">github</li>
 			</a>
-			<a
-				class="text-sm font-medium contact-links"
-				target="_blank"
-				href="https://www.linkedin.com/in/vincent-charlebois/"
-			>
-				<li class="py-2">linkedin</li>
-			</a>
-			<a class="text-sm font-medium contact-links" target="_blank" href="https://chateau.social/@v">
-				<li class="py-2">mastodon</li>
-			</a>
-			<a
-				class="text-sm font-medium contact-links"
-				target="_blank"
-				href="https://bsky.app/profile/charlebois.info"
-			>
-				<li class="py-2">bluesky</li>
-			</a>
-			<a
-				class="text-sm font-medium contact-links"
-				target="_blank"
-				href="https://www.are.na/vincent-charlebois"
-			>
-				<li class="py-2">are.na</li>
-			</a>
-			<a
-				class="text-sm font-medium contact-links"
-				target="_blank"
-				href="https://pinksky.app/profile/couleurs.bsky.social"
-			>
-				<li class="py-2">pinksky</li>
-			</a>
-			<a
-				class="text-sm font-medium contact-links"
-				target="_blank"
-				href="https://twitter.com/vincencharleboi"
-			>
-				<li class="py-2">twitter</li>
-			</a>
-			<a
-				class="text-sm font-medium contact-links"
-				target="_blank"
-				href="https://instagram.com/ffforests"
-			>
-				<li class="py-2">instagram</li>
-			</a>
-			<a
-				class="text-sm font-medium contact-links"
-				target="_blank"
-				href="https://www.vincentcharlebois.net/"
-			>
-				<li class="py-2">portfolio</li>
-			</a>
-			<a
-				class="text-sm font-medium contact-links"
-				target="_blank"
-				href="https://www.farcaster.xyz/xxc"
-			>
-				<li class="py-2">farcaster</li>
-			</a>
-
-			<a class="text-sm font-medium contact-links" target="_blank" href="https://vncnt.xyz">
-				<li class="py-2">.xyz</li>
+			<a class="text-sm font-medium contact-links" target="_blank" href="https://hypha.coop/">
+				<li class="py-2">hypha.coop</li>
 			</a>
 			<a
 				class="text-sm font-medium contact-links"
@@ -166,73 +93,26 @@
 			>
 				<li class="py-2">CV</li>
 			</a>
-			<a class="text-sm font-medium contact-links" href="/fr/portail">
-				<li class="py-2">portail</li>
+			<a
+				class="text-sm font-medium contact-links"
+				target="_blank"
+				href="mailto:vincent@charlebois.info"
+			>
+				<li class="py-2">email</li>
 			</a>
 			<a
 				class="text-sm font-medium contact-links"
 				target="_blank"
-				href="https://vincent.charlebois.info/fr/"
+				href="https://www.linkedin.com/in/vincent-charlebois/"
 			>
-				<li class="py-2">.info</li>
-			</a>
-		</ul>
-		<p class="text-2xl text-left px-5 pt-7 pb-7 mt-12 mb-1 w-80">Sites web:</p>
-		<ul class="flex flex-row flex-wrap">
-			<a
-				href="http://couleurs.tripledoublev.com"
-				class="text-sm font-medium contact-links"
-				target="_blank"
-			>
-				<li class="py-2">2025 Pbhyrhef</li>
+				<li class="py-2">linkedin</li>
 			</a>
 			<a
-				href="https://tripledoublev.com/couleur"
 				class="text-sm font-medium contact-links"
 				target="_blank"
+				href="https://bsky.app/profile/charlebois.info"
 			>
-				<li class="py-2">2024 Couleur de Montréal</li>
-			</a>
-			<a
-				href="https://vincent.charlebois.info/consecutive-days/"
-				class="text-sm font-medium contact-links"
-				target="_blank"
-			>
-				<li class="py-2">2023 Consecutive days at the studio</li>
-			</a>
-			<a href="https://postvis.io" class="text-sm font-medium contact-links" target="_blank">
-				<li class="py-2">2022 postvisions</li>
-			</a>
-			<a
-				href="https://www.couvre-feu.quebec/"
-				class="text-sm font-medium contact-links"
-				target="_blank"
-			>
-				<li class="py-2">2021 Je me souviens</li>
-			</a>
-			<a
-				href="https://vincentcharlebois.net//nodes.html"
-				class="text-sm font-medium contact-links"
-				target="_blank"
-			>
-				<li class="py-2">2018 nodes</li>
-			</a>
-			<a href="https://tripledoublev.net" class="text-sm font-medium contact-links" target="_blank">
-				<li class="py-2">2017 MCCM;nodes</li>
-			</a>
-			<a
-				href="https://tripledoublev.net/rhytidome"
-				class="text-sm font-medium contact-links"
-				target="_blank"
-			>
-				<li class="py-2">2016 Rhytidome</li>
-			</a>
-			<a
-				href="https://tripledoublev.net/scroll"
-				class="text-sm font-medium contact-links"
-				target="_blank"
-			>
-				<li class="py-2">2015 if 2 words</li>
+				<li class="py-2">bluesky</li>
 			</a>
 		</ul>
 	</div>
